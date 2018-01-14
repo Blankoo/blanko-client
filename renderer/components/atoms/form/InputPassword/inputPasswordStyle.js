58214709f4ef56c07fe03b77535f56613ffdab16
Donovan Roubos <donovan@loyals.nl>
2018-01-14 15:58:59 +0100

    Add InputPassword component with false styling

---
+++ renderer/components/atoms/form/InputPassword/inputPasswordStyle.js
@@ -0,0 +1,13 @@
+export default `
+input {
+	background-color: transparent;
+	padding: 8px 0;
+	border: none;
+	border-bottom: 1px solid #e2e2e2;
+	outline: none;
+}
+.show-hide-toggle {
+	cursor: pointer;
+	user-select: none;
+}
+`
