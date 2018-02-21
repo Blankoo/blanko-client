18b333b11b0002e7524cc648f9a559ee86fa16d6
Donovan Roubos <donovan@loyals.nl>
2018-02-21 16:41:58 +0100

    Edit ButtonStyle types

--- renderer/components/atoms/Button/ButtonStyle.js
+++ renderer/components/atoms/Button/ButtonStyle.js
@@ -13,22 +13,29 @@ button {
 	transition: 100ms linear;
 }
 
+/*Button type: default*/
 button.default:hover {
 	background-color: #1C87FB;
-	color:white;
+	box-shadow: 0 2px 8px 0 rgba(28, 135, 251, 0.33);
+	color: white;
 }
 
 button.default.active {
   background-color: #1C87FB;
-	color:white;
+	box-shadow: 0 2px 8px 0 rgba(28, 135, 251, 0.33);
+	color: white;
 }
 
+/*Button type: edit*/
 button.edit:hover {
 	background-color: #F3F3F3;
+	box-shadow: 0 2px 8px 0 rgba(243, 243, 243, 0.33);
 }
 
+/*Button type: delete*/
 button.delete:hover {
 	background-color: #B32B2B;
+	box-shadow: 0 2px 8px 0 rgba(179, 43, 43, 0.33);
 	color: #FCFCFC;
 }
 `
