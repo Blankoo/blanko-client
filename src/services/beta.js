4f28ae41beaa65ba0c9c7c4af701db22707cf11a
info@noudadrichem.com <info@noudadrichem.com>
2018-03-27 15:54:53 +0200

    post emails for beta signup

---
+++ src/services/beta.js
@@ -0,0 +1,29 @@
+import { Router as router } from 'express'
+import log from '../log'
+import BetaReleaseEmail from '../models/betaReleaseEmail'
+
+export default () => {
+  const betaReleaseEmails = router()
+
+   betaReleaseEmails.get('/', (request, response) => {
+    response.json({
+      message: 'You can signup for the beta at blankoapp.com'
+    })
+   })
+  
+   betaReleaseEmails.post('/signup', (request, response) => {
+      const { body } = request
+      
+      log.info({ body })
+
+      const newBetaSignup = new BetaReleaseEmail(body)
+
+      newBetaSignup.save(err => err
+        ? ressponse.json({ message: 'Error signin up', err })
+        : response.json({ message: 'Succesfully sign up for the beta release'})
+      )
+
+   })
+
+  return betaReleaseEmails
+}
