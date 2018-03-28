2b619cc109d494642136e538338ab590bc886eb0
info@noudadrichem.com <info@noudadrichem.com>
2018-03-28 07:39:34 +0200

    fix eslint rules + indents on beta.js

--- src/services/beta.js
+++ src/services/beta.js
@@ -5,25 +5,26 @@ import BetaReleaseEmail from '../models/betaReleaseEmail'
 export default () => {
   const betaReleaseEmails = router()
 
-   betaReleaseEmails.get('/', (request, response) => {
+  betaReleaseEmails.get('/', (request, response) => {
     response.json({
       message: 'You can signup for the beta at blankoapp.com'
     })
-   })
-  
-   betaReleaseEmails.post('/signup', (request, response) => {
-      const { body } = request
-      
-      log.info({ body })
+  })
 
-      const newBetaSignup = new BetaReleaseEmail(body)
-
-      newBetaSignup.save(err => err
-        ? ressponse.json({ message: 'Error signin up', err })
-        : response.json({ message: 'Succesfully sign up for the beta release'})
-      )
-
-   })
+  betaReleaseEmails.post('/signup', (request, response) => {
+    const { body } = request
+    const newBetaSignup = new BetaReleaseEmail(body)
+    log.info({ body })
+    newBetaSignup.save(err => err ?
+      response.json({
+        message: 'Error signin up',
+        err
+      }) :
+      response.json({
+        message: 'Succesfully sign up for the beta release'
+      })
+    )
+  })
 
   return betaReleaseEmails
 }
